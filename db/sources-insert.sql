6d1bef5ed0ab6d27c9694e07393d2dbe661d6e1f
Andrew Dushane <andrewdushane@gmail.com>
2015-12-09 10:14:48 -0600

    sources seed

---
+++ db/sources-insert.sql
@@ -0,0 +1,23 @@
+\c simply_news
+
+INSERT INTO sources (name, rss_url, homepage_url, image_url ) VALUES (
+  ('The Verge', 'http://www.theverge.com/rss/index.xml', 'http://www.theverge.com/', 'the-verge.png'),
+  ('Mashable', 'http://feeds.mashable.com/Mashable', 'http://mashable.com/', 'mashable.png'),
+  ('Ars Technica', 'http://feeds.arstechnica.com/arstechnica/index', 'http://arstechnica.com/', 'ars-technica.png'),
+  ('Los Angeles Times', 'http://www.latimes.com/rss2.0.xml', 'http://www.latimes.com/', 'los-angeles-times.png'),
+  ('BBC', 'http://feeds.bbci.co.uk/news/rss.xml', 'http://www.bbc.com/news', 'bbc-news.png'),
+  ('NPR', 'http://www.npr.org/rss/rss.php?id=1001', 'http://www.npr.org/sections/news/', 'npr.png'),
+  ('Reuters', 'http://feeds.reuters.com/reuters/topNews', 'http://www.reuters.com/news', 'reuters.png'),
+  ('CNN', 'http://rss.cnn.com/rss/cnn_topstories.rss', 'http://www.cnn.com/', 'cnn.jpg'),
+  ('ESPN', 'http://sports.espn.go.com/espn/rss/news', 'http://espn.go.com/', 'espn.png'),
+  ('Wall Street Journal', 'http://www.wsj.com/xml/rss/3_7085.xml', 'http://www.wsj.com/', 'wsj.png'),
+  ('Financial Times', 'http://www.ft.com/rss/home/us', 'http://www.ft.com/home/us', 'financial-times.png'),
+  ('Yahoo News', 'http://news.yahoo.com/rss/', 'http://news.yahoo.com/', 'yahoo.png'),
+  ('The New Yorker', 'http://www.newyorker.com/feed/news', 'http://www.newyorker.com/', 'the-new-yorker.png'),
+  ('New York Review of Books', 'http://feeds.feedburner.com/nybooks', 'http://www.nybooks.com/', 'nyrb.png'),
+  ('Elle', 'http://www.elle.com/rss/all.xml', 'http://www.elle.com/', 'elle.png'),
+  ('Glamour', 'http://feeds.glamour.com/glamour/glamour_all', 'http://www.glamour.com/', 'glamour.png'),
+  ('Hollywood Reporter', 'http://feeds.feedburner.com/thr/news', 'http://www.hollywoodreporter.com/', 'hollywood-reporter.png'),
+  ('Yahoo Sports', 'https://sports.yahoo.com/top/rss.xml', 'http://sports.yahoo.com/', 'yahoo-sports.png'),
+  ('Hacker News', 'https://news.ycombinator.com/rss', 'https://news.ycombinator.com/', 'hacker-news.png')
+)

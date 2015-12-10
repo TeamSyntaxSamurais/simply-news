17e4f9aaa9840fdf7e59431d4b3c1a7768ac3e71
Andrew Dushane <andrewdushane@gmail.com>
2015-12-10 11:44:04 -0600

    mysql2 adapter

--- controllers/application.rb
+++ controllers/application.rb
@@ -5,7 +5,7 @@ class ApplicationController < Sinatra::Base
 
   ActiveRecord::Base.establish_connection(
       :database => 'simply_news',
-      :adapter => 'mysql'
+      :adapter => 'mysql2'
   )
 
   set :views, File.expand_path('../../views', __FILE__)

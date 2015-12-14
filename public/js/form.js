1898d25d5db4bb20cdbf87b944b4f7dc73885611
Andrew Dushane <andrewdushane@gmail.com>
2015-12-14 12:36:57 -0600

    check/x news sources

--- public/js/form.js
+++ public/js/form.js
@@ -2,6 +2,17 @@ $('document').ready( function() {
   // highlight checked sources
   $('input[type="checkbox"]:checked').prev().addClass('checked-image');
   // toggle check on checkbox when image is clicked
+  function checkUncheck() {
+    var xCircle = $('<i class="fa fa-times-circle-o"></i>');
+    var unchecked = $('.form-image').not('.checked-image').next();
+    unchecked.next().remove();
+    xCircle.insertAfter(unchecked);
+    var vCircle = $('<i class="fa fa-check-circle-o"></i>');
+    var checked = $('.form-image.checked-image').next();
+    checked.next().remove();
+    vCircle.insertAfter(checked);
+  }
+  checkUncheck();
   $(".form-image").click( function() {
     var checkbox = $(this).next();
     checkbox.trigger('click');
@@ -10,6 +21,7 @@ $('document').ready( function() {
     } else {
       $(this).removeClass('checked-image');
     }
+    checkUncheck();
   });
   $('.close-overlay').click( function() {
     $('#overlay-outer').hide();
